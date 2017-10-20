b7a9258752ba3ec38233f05f818f4e1402855d52
Administrator <Administrator@PC-20170504PCLA>
2017-10-20 17:28:48 +0800

    添加项目文件。

---
+++ DALAccess/DBUtility/PubConstant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Configuration;
+
+namespace Maticsoft.DBUtility
+{
+    public class PubConstant
+    {
+        /// <summary>
+        /// 获取连接字符串
+        /// </summary>
+        public static string ConnectionString
+        {
+            get
+            {
+                return ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString.ToString();
+            }
+        }
+    }
+}
